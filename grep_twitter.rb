bee7ca3de19c83b3000a1e0766510bae790abc47
Mike Ottum <mike.ottum@gmail.com>
2012-02-10 21:44:04 -0800

    Query for changes since the last Tweet id seen

--- grep_twitter.rb
+++ grep_twitter.rb
@@ -1,6 +1,14 @@
+#!/usr/bin/ruby
+
 require 'rubygems'
 require 'twitter'
+require 'ruby-debug'
 
-Twitter.user_timeline(ARGV[1]).each do |tweet|
-  print "#{tweet.text}\n" if (tweet.text =~ /#{ARGV[0]}/i)
+regex = $ARGV[0]
+last_tweet_id = $ARGV[1]
+
+Twitter.list_timeline("ottumm", "food-trucks", {:since_id => (last_tweet_id or 1)}).each do |tweet|
+  if (tweet.text =~ /#{regex}/i)
+    puts "@#{tweet.user.screen_name} (#{tweet.id}): #{tweet.text}"
+  end
 end
