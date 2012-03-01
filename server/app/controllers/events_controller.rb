809569f8aa8f22fb58b94ff801c1c40c8e2f0005
Mike Ottum <mike.ottum@gmail.com>
2012-02-29 23:41:28 -0800

    Only show today's food trucks on /events

--- server/app/controllers/events_controller.rb
+++ server/app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.find_today
 
     respond_to do |format|
       format.html # index.html.erb
--- server/app/models/event.rb
+++ server/app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ActiveRecord::Base
+
+	def self.find_today
+		set_time_zone
+		where :start_time => (Time.now.beginning_of_day..Time.now.beginning_of_day + 1.day)
+	end
+
+	protected
+
+	def self.set_time_zone
+		Time.zone = "Pacific Time (US & Canada)"
+	end
 end
