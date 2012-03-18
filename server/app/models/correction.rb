38131ebacb47cd5a2420187e592221772f1b4469
Mike Ottum <mike.ottum@gmail.com>
2012-03-18 11:16:48 -0700

    Validate correction form fields

--- server/app/models/correction.rb
+++ server/app/models/correction.rb
@@ -2,4 +2,11 @@ class Correction < ActiveRecord::Base
 	belongs_to :event
 
 	validates_associated :event
+	validates_presence_of :event
+	validates_presence_of :start_time
+	validates_presence_of :end_time
+	validates_presence_of :location
+	validates_presence_of :latitude
+	validates_presence_of :longitude
+	validates_presence_of :formatted_address
 end
